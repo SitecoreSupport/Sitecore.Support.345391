22726e77411ee17ffe04f20e719238b812a2f5ca
EOP <eop@sitecore.com>
2019-07-22 16:20:07 +0300

    default code

---
+++ src/Sitecore.Support.345391/ExperienceEditor/Speak/Ribbon/Requests/LockItem/CanToggleLockRequest.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
+{
+    public class CanToggleLockRequest : PipelineProcessorControlStateRequest<ItemContext>
+    {
+        public override bool GetControlState()
+        {
+            base.RequestContext.ValidateContextItem();
+            return this.CanLock(base.RequestContext.Item);
+        }
+
+        private bool CanLock(Item item)
+        {
+            return !(!TemplateManager.IsFieldPartOfTemplate(FieldIDs.Workflow, item) | !TemplateManager.IsFieldPartOfTemplate(FieldIDs.WorkflowState, item)) && item.Access.CanWrite() && item.Access.CanWriteLanguage() && (Sitecore.Context.IsAdministrator || !item.Locking.IsLocked() || item.Locking.HasLock()) && (item.Locking.CanLock() | item.Locking.CanUnlock()) && !item.Appearance.ReadOnly;
+        }
+    }
+}
---
+++ src/Sitecore.Support.345391/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.345391")]
+[assembly: AssemblyProduct("Sitecore.Support.345391")]
+[assembly: ComVisible(false)]
