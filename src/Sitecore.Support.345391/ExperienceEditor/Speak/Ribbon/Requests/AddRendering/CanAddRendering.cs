607c749e3020dcefabcdd0b83d9cb009cc4d1e0a
EOP <eop@sitecore.com>
2019-07-24 19:29:57 +0300

    enabling "Add Rendering" and "Layout Details" buttons in EXM for any user

---
+++ src/Sitecore.Support.345391/ExperienceEditor/Speak/Ribbon/Requests/AddRendering/CanAddRendering.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Utils;
+using Sitecore.SecurityModel;
+using Sitecore.Web;
+using Sitecore.Web.UI.HtmlControls;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.AddRendering
+{
+    public class CanAddRendering : PipelineProcessorControlStateRequest<ItemContext>
+    {
+        public override bool GetControlState()
+        {
+            if (!base.RequestContext.Site.EnableWebEdit | !Policy.IsAllowed("Page Editor/Can Design"))
+            {
+                return false;
+            }
+            base.RequestContext.ValidateContextItem();
+            #region sitecore.support.345391
+            //if (base.RequestContext.WebEditMode != "edit" || !WebEditUtil.CanDesignItem(base.RequestContext.Item) || base.RequestContext.Item.IsFallback || ItemUtility.RequireLockToEdit(base.RequestContext.Item))
+            if ((base.RequestContext.WebEditMode != "edit" || !WebEditUtil.CanDesignItem(base.RequestContext.Item) || base.RequestContext.Item.IsFallback || ItemUtility.RequireLockToEdit(base.RequestContext.Item))&& (base.RequestContext.Item.Name!="Message Root")||base.RequestContext.Item.Branch==null)
+            #endregion sitecore.support.345391
+            {
+                return false;
+            }
+            string str = "design";
+            string @string = Registry.GetString("/Current_User/Page Editor/Capability/" + str, Sitecore.ExperienceEditor.Constants.Registry.CheckboxTickedRegistryValue);
+            return !(@string == Sitecore.ExperienceEditor.Constants.Registry.CheckboxUnTickedRegistryValue);
+        }
+    }
+}
---
+++ src/Sitecore.Support.345391/ExperienceEditor/Speak/Ribbon/Requests/LayoutDetails/CanEditLayoutDetails.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Utils;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LayoutDetails
+{
+    public class CanEditLayoutDetailsRequest : PipelineProcessorControlStateRequest<ItemContext>
+    {
+        public override bool GetControlState()
+        {
+            base.RequestContext.ValidateContextItem();
+            return this.GetCommandState();
+        }
+
+        public virtual bool GetCommandState()
+        {
+            Item item = base.RequestContext.Item;
+            #region sitecore.support.345391
+            //return TemplateManager.IsFieldPartOfTemplate(FieldIDs.LayoutField, item) && !(base.RequestContext.WebEditMode == "preview") && !base.RequestContext.Item.IsFallback && item.Access.CanWrite() && item.Access.CanWriteLanguage() && !ItemUtility.RequireLockToEdit(item);
+            return (TemplateManager.IsFieldPartOfTemplate(FieldIDs.LayoutField, item) && !(base.RequestContext.WebEditMode == "preview") && !base.RequestContext.Item.IsFallback && item.Access.CanWrite() && item.Access.CanWriteLanguage() && !ItemUtility.RequireLockToEdit(item)) || System.Web.HttpContext.Current.Request.Path.Contains("sitecore/client/Applications/ECM/");
+            #endregion sitecore.support.345391
+        }
+    }
+}
